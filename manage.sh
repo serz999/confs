f5aa51e0cbbf955f683836cbfb4d625c4d8702c5
serz999 <serz999@mail.ru>
2025-01-05 01:10:13 +0300

    fix manage.sh

--- manage.sh
+++ manage.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-CONFS=$HOME/configs
+CONFS=$HOME/confs
 DOTCONFIG=$HOME/.config
 
 cd $CONFS
 
 if [ "$1" = "install" ]; then
-    read -p "It will overwrite all overlaping configs. Are you sure want to install configuration onto this system? [y/N]" -n 1 -r
+    read -p "  It will overwrite all overlaping configs. Are you sure want to install configuration onto this system? [y/N]" -n 1 -r
     echo
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         exit 1
     fi
 
+    echo "   ..."
+
     cp -r i3 nvim $DOTCONFIG
     cp .Xresources $HOME
+
+    echo "  Done."
 elif [ "$1" = "write" ]; then
     cp -r $DOTCONFIG/i3 $DOTCONFIG/nvim $HOME/.Xresources .
 else
-    echo "Only these commands are presented:"
-    echo "  install"
+    echo "  COMMANDS"
+    echo
+    echo "    install"
     echo "      install configuration from repo onto system"
-    echo "  write"
+    echo
+    echo "    write"
     echo "      write configs from system to this repo"
+    echo
+fi
